cbab9f1e8aa7e6926e72dbf649de9c368bf36bec
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-07-17 01:40:26 +0900

    ..

--- game/cgame.c
+++ game/cgame.c
@@ -1,1 +1,16 @@
 #include "cgame.h"
+#include "core/cmemory.h"
+
+CGame* newCGame(CMemory* mem) {
+  CGame * ret = cmemory_calloc(mem, 1, sizeof(CGame));
+  ret->parent.cmemory = mem;
+  return ret;
+}
+
+CGame* initCGame(CGame* obj, char* name, char *title, int width, int height) {
+  initCObject((CObject*)obj, name);
+  snprintf(obj->title, sizeof(obj->title), "%s", title);
+  obj->width = width;
+  obj->height = height;
+  return obj;
+}
--- game/cgame.h
+++ game/cgame.h
@@ -9,6 +9,12 @@
 
 typedef struct {
   CObject parent;
+  char title[256];
+  int width;
+  int height;
 } CGame;
+
+CGame* newCGame(CMemory* mem);
+CGame* initCGame(CGame*, char* name,char* title, int width, int height);
 
 #endif
