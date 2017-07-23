96254f7e3e29285beba0fc98743dd161e0b9702b
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-07-23 16:39:43 +0900

    ..

--- core/cobject.c
+++ core/cobject.c
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include "cmemory.h"
 
+int _hashCode(void *obj);
+int _compareTo(void *obj, void *src);
+
 CObject* newCObject(CMemory* cmemory) {
   CObject *ret = cmemory_calloc(cmemory, 1,sizeof(CObject));
   ret->cmemory = cmemory;
@@ -13,14 +16,30 @@ CObject* initCObject(CObject*obj, const char *name) {
   snprintf(obj->name, sizeof(obj->name), "%s", name);
   obj->reference = 1;
   obj->funcFree = freeCObject;
+  obj->funcHashCode = _hashCode;
+  obj->funcCompareTo = _compareTo;
   onMode(obj, COBJECT_MODE_FREEABLE);
   return obj;
 }
 
 void freeCObject(void* obj) {
-
   if(obj != NULL) {
     cmemory_free(((CObject*)obj)->cmemory, obj);
+  }
+}
+
+int _hashCode(void *obj) {
+  return 0;
+}
+
+int _compareTo(void *obj, void *src) {
+  if(obj == src) {
+    return 0;
+  }
+  if(obj > src) {
+    return 1;
+  } else {
+    return-1;
   }
 }
 
@@ -52,6 +71,21 @@ int getMode(CObject* obj, int mode) {
   }
 }
 
+int cobject_hashCode(CObject* obj) {
+  CObjectFuncHashCode func = obj->funcHashCode;
+  if(func != NULL) {
+    return func(obj);
+  }
+  return 0;
+}
+
+int cobject_compareTo(CObject* obj, CObject* src) {
+  CObjectFuncCompareTo func = obj->funcCompareTo;
+  if(func != NULL) {
+    return func(obj, src);
+  }
+  return 0;
+}
 
 CObject* _releaseCObject(CObject* obj, int isForce) {
   obj->reference--;
--- core/cobject.h
+++ core/cobject.h
@@ -8,12 +8,17 @@
 #define COBJECT_NAME "obj"
 
 typedef void (*CObjectFuncFree)(void *obj);
+typedef int (*CObjectFuncHashCode)(void *obj);
+typedef int (*CObjectFuncCompareTo)(void *obj, void *src);
+
 typedef struct {
   char name[8];
   int reference;
   int mode;
   int index;
   CObjectFuncFree funcFree;
+  CObjectFuncHashCode funcHashCode;
+  CObjectFuncCompareTo funcCompareTo;
   CMemory* cmemory;
 } CObject;
 
@@ -28,5 +33,7 @@ CObject* releaseCObject(CObject* obj);
 CObject* releaseForceCObject(CObject* obj);
 CObject* onMode(CObject* obj, int mode);
 CObject* offMode(CObject* obj, int mode);
+int cobject_hashCode(CObject* obj);
+int cobject_compareTo(CObject* obj, CObject* src);
 int getMode(CObject* obj, int mode);
 #endif
