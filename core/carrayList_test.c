0da74fc5870ef926780e4dba1f1a997174a583ab
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-07-26 03:13:28 +0900

    ..

--- core/carrayList_test.c
+++ core/carrayList_test.c
@@ -16,43 +16,46 @@ void carrayList_test(){
 
   carrayList_addLast(lin, cobject_downCounter(initCObject(newCObject(mem), "test1")));
   carrayList_addLast(lin, cobject_downCounter(initCObject(newCObject(mem), "test2")));
-/*  clinkedList_insert(lin, cobject_downCounter(initCObject(newCObject(mem), "test")),1);
+  carrayList_addLast(lin, cobject_downCounter(initCObject(newCObject(mem), "test3")));
 
-  if(clinkedList_getLength(lin) != 3) {
-    printf("  NG1 : length = %d\r\n", clinkedList_getLength(lin));
+  if(carrayList_getLength(lin) != 3) {
+    printf("  NG : length = %d\r\n", carrayList_getLength(lin));
     passed = 0;
   }
 
-  if(strcmp(clinkedList_get(lin,0)->name, "test1") != 0) {
-    printf("  NG2 %s: \r\n", clinkedList_get(lin,0)->name);
+  if(strcmp(carrayList_get(lin,0)->name, "test1") != 0) {
+    printf("  NG2 %s: \r\n", carrayList_get(lin,0)->name);
     passed = 0;
   }
 
-  if(strcmp(clinkedList_get(lin,1)->name, "test") != 0) {
-    printf("  NG3_2 %s: \r\n",clinkedList_get(lin,2)->name);
-    printf("  NG3_1 %s: \r\n", clinkedList_get(lin,1)->name);
-    printf("  NG3_0 %s: \r\n", clinkedList_get(lin,0)->name);
+  if(strcmp(carrayList_get(lin,1)->name, "test2") != 0) {
+    printf("  NG3_2 %s: \r\n", carrayList_get(lin,2)->name);
+    printf("  NG3_1 %s: \r\n", carrayList_get(lin,1)->name);
+    printf("  NG3_0 %s: \r\n", carrayList_get(lin,0)->name);
     passed = 0;
   }
 
-  if(strcmp(clinkedList_get(lin,2)->name, "test2") != 0) {
+  if(strcmp(carrayList_get(lin,2)->name, "test3") != 0) {
     printf("  NG4 : \r\n");
     passed = 0;
   }
 
-  clinkedList_remove(lin, 0);
-  if(clinkedList_getLength(lin) != 2) {
-    printf("  NG5 : length = %d\r\n", clinkedList_getLength(lin));
+  carrayList_set(lin, 0, NULL);
+  if(carrayList_getLength(lin) != 3) {
+    printf("  NG : length = %d\r\n", carrayList_getLength(lin));
+    passed = 0;
+  }
+  carrayList_removeLast(lin);
+  if(carrayList_getLength(lin) != 2) {
+    printf("  NG : length = %d\r\n", carrayList_getLength(lin));
     passed = 0;
   }
 
-  if(strcmp(clinkedList_get(lin,0)->name, "test") != 0) {
-    printf("  NG6_1 %s: \r\n", clinkedList_get(lin,1)->name);
-    printf("  NG6_0 %s: \r\n", clinkedList_get(lin,0)->name);
+  if(strcmp(carrayList_get(lin,1)->name, "test2") != 0) {
+    printf("  NG6_1 : \r\n");
     passed = 0;
   }
 
-*/
   releaseCObject((CObject*)lin);
   if(mem->callocCounter != mem->freeCounter) {
     printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
