2931a38dcaab88dab9f0e9cdc8c39be20120cf6b
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-07-26 02:43:45 +0900

    ..

---
+++ core/carrayList_test.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <string.h>
+#include "cmemory.h"
+#include "clinkedList.h"
+
+void carrayList_test(){
+  printf("# clinkedList_test\n");
+  int passed = 1;
+  CMemory *mem = initCMemory(newCMemory());
+  CLinkedList *lin = initCLinkedList(newCLinkedList(mem));
+
+  if(clinkedList_getLength(lin) != 0) {
+    printf("  NG : length = %d\r\n", clinkedList_getLength(lin));
+    passed = 0;
+  }
+  clinkedList_addLast(lin, cobject_downCounter(initCObject(newCObject(mem), "test1")));
+  clinkedList_addLast(lin, cobject_downCounter(initCObject(newCObject(mem), "test2")));
+  clinkedList_insert(lin, cobject_downCounter(initCObject(newCObject(mem), "test")),1);
+
+  if(clinkedList_getLength(lin) != 3) {
+    printf("  NG1 : length = %d\r\n", clinkedList_getLength(lin));
+    passed = 0;
+  }
+
+  if(strcmp(clinkedList_get(lin,0)->name, "test1") != 0) {
+    printf("  NG2 %s: \r\n", clinkedList_get(lin,0)->name);
+    passed = 0;
+  }
+
+  if(strcmp(clinkedList_get(lin,1)->name, "test") != 0) {
+    printf("  NG3_2 %s: \r\n",clinkedList_get(lin,2)->name);
+    printf("  NG3_1 %s: \r\n", clinkedList_get(lin,1)->name);
+    printf("  NG3_0 %s: \r\n", clinkedList_get(lin,0)->name);
+    passed = 0;
+  }
+
+  if(strcmp(clinkedList_get(lin,2)->name, "test2") != 0) {
+    printf("  NG4 : \r\n");
+    passed = 0;
+  }
+
+  clinkedList_remove(lin, 0);
+  if(clinkedList_getLength(lin) != 2) {
+    printf("  NG5 : length = %d\r\n", clinkedList_getLength(lin));
+    passed = 0;
+  }
+
+  if(strcmp(clinkedList_get(lin,0)->name, "test") != 0) {
+    printf("  NG6_1 %s: \r\n", clinkedList_get(lin,1)->name);
+    printf("  NG6_0 %s: \r\n", clinkedList_get(lin,0)->name);
+    passed = 0;
+  }
+
+
+  releaseCObject((CObject*)lin);
+  if(mem->callocCounter != mem->freeCounter) {
+    printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
+    passed = 0;
+  }
+
+  freeCMemory(mem);
+  if(passed) {
+    printf("  OK : \r\n");
+  }
+  printf("\r\n\r\n");
+}
--- core/test.c
+++ core/test.c
@@ -8,11 +8,7 @@
 extern void cbytes_test();
 extern void cstring_test();
 extern void clinkedList_test();
-
-int test01();
-int test02();
-int test03a();
-int test03b();
+extern void carrayList_test();
 
 int main(int argv, char** argc) {
   CObjectManager *objMgr = getCObjectManager();
@@ -23,94 +19,10 @@ int main(int argv, char** argc) {
   cbytes_test();
   cstring_test();
   clinkedList_test();
+  carrayList_test();
 //  test01();
 //  test02();
 //  test03a();
   //test03b();*/
-  return 0;
-}
-
-int test01() {
-  CObjectManager *objMgr = getCObjectManager();
-  CObject *obj = initCObject(newCObject(getCMemory()),"test object");
-  objectManager_addObject(objMgr, obj);
-  releaseCObject(obj);
-  objectManager_showInfo(objMgr);
-  objectManager_releaseObject(objMgr, obj);
-  objectManager_showInfo(objMgr);
-  printf("[TEST]\n");
-  printf("mem: expect  %d == %d \n\n",
-    getCMemory()->callocCounter,
-    getCMemory()->freeCounter);
-
-  return 0;
-}
-
-int test02() {
-  printf("[TEST02]\n");
-  CArrayList *objArray = initCArrayList(newCArrayList(getCMemory()), 10);
-  CArrayList_addLast(objArray, cobject_downCounter(initCObject(newCObject(getCMemory()), "obj00")));
-  CArrayList_addLast(objArray, cobject_downCounter(initCObject(newCObject(getCMemory()), "obj01")));
-  for(int i=0;i<objArray->length;i++) {
-    printf("a- %d %s (%d)\n", i, objArray->objects[i]->name,objArray->objects[i]->reference);
-  }
-  for(int i=0;i<objArray->length;i++) {
-    CObject *tmp = objArray->objects[i];
-    printf("b- %d %s (%d)\n", i, tmp->name, tmp->reference);
-    CArrayList_set(objArray, i, NULL);
-  }
-  releaseCObject((CObject*)objArray);
-  printf("mem: expect  %d == %d \n",
-    getCMemory()->callocCounter,
-    getCMemory()->freeCounter);
-  printf("\n");
-  return 0;
-}
-
-int test03a() {
-  CLinkedList *obj = initCLinkedList(newCLinkedList(getCMemory()));
-  printf("[TEST03] %d\n",obj->parent.reference );
-
-//  offMode((CObject*)obj, COBJECT_MODE_FREEABLE);
-  releaseCObject((CObject*)obj);
-  /*
-  if(obj->parent.reference <= 0) {
-    releaseForceCObject((CObject*)obj);
-  }*/
-  printf("mem: expect  %d == %d \n",getCMemory()->callocCounter, getCMemory()->freeCounter);
-  return 0;
-}
-
-int test03b() {
-  //
-  //
-  CLinkedList *obj = initCLinkedList(newCLinkedList(getCMemory()));
-  printf("%d", obj->length);
-  clinkedList_addLast(obj, cobject_downCounter(initCObject(newCObject(getCMemory()),"test00")));
-  clinkedList_addLast(obj, cobject_downCounter(initCObject(newCObject(getCMemory()),"test01")));
-//  downCounter(o);
-//  printf("===%d\n", o->reference);
-/*
-  printf("[A]%d", obj->length);
-  for(int i=0;i<obj->length;i++) {
-    printf("%s\r\n", linkedList_get(obj, i)->name);
-  }
-
-  linkedList_addLast(obj, downCounter(initCObject(newCObject(getCMemory()),"test01")));
-  printf("[B]%d", obj->length);
-  for(int i=0;i<obj->length;i++) {
-    printf("%s %d \r\n", linkedList_get(obj, i)->name, linkedList_get(obj, i)->reference);
-  }
-
-  linkedList_removeLast(obj);
-  */
-  printf("[C12]%d\r\n", obj->length);
-  for(int i=0;i<obj->length;i++) {
-    printf("## %s %d \r\n", clinkedList_get(obj, i)->name, clinkedList_get(obj, i)->reference);
-  }
-  printf("[ZZZ]\n");
-  releaseCObject((CObject*)obj);
-  printf("mem: expect  %d == %d \n",getCMemory()->callocCounter, getCMemory()->freeCounter);
-//  printf("===%d\n", o->reference);
   return 0;
 }
