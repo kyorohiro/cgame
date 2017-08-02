ccd013288855050c86d306ddcfd0f29e21e39687
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-03 07:49:45 +0900

    ..

--- game/cgame.c
+++ game/cgame.c
@@ -3,18 +3,7 @@
 #include "core/cstring.h"
 #include "core/cbytesBuilder.h"
 
-#define GL_GLEXT_PROTOTYPES
-#define EGL_EGLEXT_PROTOTYPES
-
-#include <stdio.h>
-#include <stdlib.h>
-#ifdef __APPLE__
-#include <OpenGL/gl.h>
-#include <Glut/glut.h>
-#else
-#include <GL/gl.h>
-#include <GL/glut.h>
-#endif
+#include "cglutil.h"
 
 void cgame_draw(void);
 
