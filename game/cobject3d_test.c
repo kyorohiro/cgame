8f029e50df5ae19c9f676ede688f5233056a21d4
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-05 22:50:51 +0900

    ..

---
+++ game/cobject3d_test.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "core/cmemory.h"
+#include "core/clinkedList.h"
+#include "core/cbytes.h"
+#include "cobject3d.h"
+
+void cobject3d_test(){
+  printf("cobject3d_test\r\n");
+  int passed = 1;
+  CMemory *mem = initCMemory(newCMemory());
+  CObject3D *bye = initCObject3D(newCObject3D(mem));
+
+  //releaseCObject((CObject*)bye);
+  if(mem->callocCounter != mem->freeCounter) {
+    printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
+    passed = 0;
+  }
+
+  freeCMemory(mem);
+  if(passed) {
+    printf("  OK : \r\n");
+  }
+  printf("\r\n\r\n");
+}
--- game/test.c
+++ game/test.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 
-
+extern void cobject3d_test();
 int main(int argc, char** argv) {
+  cobject3d_test();
   return 0;
 }
