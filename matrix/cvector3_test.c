3fff3ab1efe33a89144bc664dd39a2de6f016ac0
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-09 07:37:07 +0900

    ..

---
+++ matrix/cvector3_test.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "core/cbytes.h"
+#include "core/cmemory.h"
+#include "cmatrix4.h"
+#include "cvector4.h"
+
+void cvector3_test_add() {
+  printf("# cvector3_test_add\n");
+  int passed = 1;
+  CMemory *mem = initCMemory(newCMemory());
+  CVector4 *vec = initCVector4(newCVector4(mem),0.0,0.0,0.0,0.0);
+  releaseCObject((CObject*)vec);
+  if(mem->callocCounter != mem->freeCounter) {
+    printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
+    passed = 0;
+  }
+
+  freeCMemory(mem);
+  if(passed) {
+    printf("  OK : \r\n");
+  }
+  printf("\r\n\r\n");
+}
--- matrix/test.c
+++ matrix/test.c
@@ -5,11 +5,13 @@
 extern void cmatrix4_test_add();
 extern void cmatrix4_test_outer();
 extern void cvector4_test_add();
+extern void cvector3_test_add();
 int test01();
 int main(int argv, char** argc) {
   printf("hello world");
   cmatrix4_test_add();
   cmatrix4_test_outer();
   cvector4_test_add();
+  cvector3_test_add();
   return 0;
 }
