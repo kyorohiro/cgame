00b55c6f30a99835067b31c6ba1c5e747565e9d9
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-09 07:58:26 +0900

    ..

--- matrix/cvector3_test.c
+++ matrix/cvector3_test.c
@@ -2,13 +2,20 @@
 #include "core/cbytes.h"
 #include "core/cmemory.h"
 #include "cmatrix4.h"
-#include "cvector4.h"
+#include "cvector3.h"
 
 void cvector3_test_add() {
   printf("# cvector3_test_add\n");
   int passed = 1;
   CMemory *mem = initCMemory(newCMemory());
-  CVector4 *vec = initCVector4(newCVector4(mem),0.0,0.0,0.0,0.0);
+  CVector3 *vec = initCVector3(newCVector3(mem), 1.0, 2.0, 3.0);
+
+  if(vec->value[0] != 1.0 || vec->value[1] != 2.0 || vec->value[2] != 3.0 ) {
+    printf("  NG : failed to init value \r\n");
+    passed = 0;
+  }
+
+
   releaseCObject((CObject*)vec);
   if(mem->callocCounter != mem->freeCounter) {
     printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
