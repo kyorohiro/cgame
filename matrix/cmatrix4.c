1eef93c24ec1962b3da78c9c243dad24cfb35084
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-09 01:28:35 +0900

    ..

--- matrix/cmatrix4.c
+++ matrix/cmatrix4.c
@@ -74,23 +74,23 @@ CMatrix4* cmatrix4_outer(CMatrix4* obj, CMatrix4* arg, CMatrix4* out) {
 //
 CMatrix4* cmatrix4_setIdentity(CMatrix4* obj) {
   obj->value[0] = 1.0;
-  obj->value[1] = 0.0;
-  obj->value[2] = 0.0;
-  obj->value[3] = 0.0;
-
   obj->value[4] = 0.0;
-  obj->value[5] = 1.0;
-  obj->value[6] = 0.0;
-  obj->value[7] = 0.0;
-
   obj->value[8] = 0.0;
-  obj->value[9] = 0.0;
-  obj->value[10] = 1.0;
-  obj->value[11] = 0.0;
-
   obj->value[12] = 0.0;
+
+  obj->value[1] = 0.0;
+  obj->value[5] = 1.0;
+  obj->value[9] = 0.0;
   obj->value[13] = 0.0;
-  obj->value[14] = 0.0;
+
+  obj->value[2] = 0.0;
+  obj->value[6] = 0.0;
+  obj->value[10] = 1.0;
+  obj->value[13] = 0.0;
+
+  obj->value[3] = 0.0;
+  obj->value[7] = 0.0;
+  obj->value[11] = 0.0;
   obj->value[15] = 1.0;
 
   return obj;
@@ -131,23 +131,23 @@ CMatrix4* cmatrix4_setRotationY(CMatrix4* obj, double radians){
   double c = cos(radians);
   double s = sin(radians);
   obj->value[0] = c;
-  obj->value[1] = 0.0;
-  obj->value[2] = -s;
-  obj->value[3] = 0.0;
-
   obj->value[4] = 0.0;
-  obj->value[5] = 1.0;
-  obj->value[6] = 0.0;
-  obj->value[7] = 0.0;
-
-  obj->value[8] =  s;
-  obj->value[9] = 0.0;
-  obj->value[10] = c;
-  obj->value[11] = 0.0;
-
+  obj->value[8] = s;
   obj->value[12] = 0.0;
+
+  obj->value[1] = 0.0;
+  obj->value[5] = 1.0;
+  obj->value[9] = 0.0;
   obj->value[13] = 0.0;
+
+  obj->value[2] =  -s;
+  obj->value[6] = 0.0;
+  obj->value[10] = c;
   obj->value[14] = 0.0;
+
+  obj->value[3] = 0.0;
+  obj->value[7] = 0.0;
+  obj->value[11] = 0.0;
   obj->value[15] = 1.0;
   return obj;
 }
@@ -160,23 +160,23 @@ CMatrix4* cmatrix4_setRotationX(CMatrix4* obj, double radians){
   double s = sin(radians);
 
   obj->value[0] = 1;
-  obj->value[1] = 0.0;
-  obj->value[2] = 0.0;
-  obj->value[3] = 0.0;
-
   obj->value[4] = 0.0;
-  obj->value[5] = c;
-  obj->value[6] = s;
-  obj->value[7] = 0.0;
-
   obj->value[8] = 0.0;
-  obj->value[9] = -s;
-  obj->value[10] = c;
-  obj->value[11] = 0.0;
-
   obj->value[12] = 0.0;
+
+  obj->value[1] = 0.0;
+  obj->value[5] = c;
+  obj->value[9] = -s;
   obj->value[13] = 0.0;
+
+  obj->value[2] = 0.0;
+  obj->value[6] =  s;
+  obj->value[10] = c;
   obj->value[14] = 0.0;
+
+  obj->value[3] = 0.0;
+  obj->value[7] = 0.0;
+  obj->value[11] = 0.0;
   obj->value[15] = 1.0;
   return obj;
 }
@@ -188,69 +188,69 @@ CMatrix4* cmatrix4_setRotationZ(CMatrix4* obj, double radians){
   double c = cos(radians);
   double s = sin(radians);
   obj->value[0] = c;
-  obj->value[1] = s;
-  obj->value[2] = 0.0;
-  obj->value[3] = 0.0;
-
   obj->value[4] = -s;
-  obj->value[5] = c;
-  obj->value[6] = 0.0;
-  obj->value[7] = 0.0;
-
   obj->value[8] = 0.0;
-  obj->value[9] = 0.0;
-  obj->value[10] = 1.0;
-  obj->value[11] = 0.0;
-
   obj->value[12] = 0.0;
+
+  obj->value[1] = s;
+  obj->value[5] = c;
+  obj->value[9] = 0.0;
   obj->value[13] = 0.0;
+
+  obj->value[2] = 0.0;
+  obj->value[6] = 0.0;
+  obj->value[10] = 1.0;
   obj->value[14] = 0.0;
+
+  obj->value[3] = 0.0;
+  obj->value[7] = 0.0;
+  obj->value[11] = 0.0;
   obj->value[15] = 1.0;
   return obj;
 }
 
 CMatrix4* cmatrix4_setTranslation(CMatrix4* obj, double x, double y, double z) {
   obj->value[0] = 1.0;
-  obj->value[1] = 0.0;
-  obj->value[2] = 0.0;
-  obj->value[3] = x;
-
   obj->value[4] = 0.0;
-  obj->value[5] = 1.0;
-  obj->value[6] = 0.0;
-  obj->value[7] = y;
-
   obj->value[8] = 0.0;
-  obj->value[9] = 0.0;
-  obj->value[10] = 1.0;
-  obj->value[11] = z;
+  obj->value[12] = x;
 
-  obj->value[12] = 0.0;
-  obj->value[13] = 0.0;
-  obj->value[14] = 0.0;
+  obj->value[1] = 0.0;
+  obj->value[5] = 1.0;
+  obj->value[9] = 0.0;
+  obj->value[13] = y;
+
+  obj->value[2] = 0.0;
+  obj->value[6] = 0.0;
+  obj->value[10] = 1.0;
+  obj->value[14] = z;
+
+  obj->value[3] = 0.0;
+  obj->value[7] = 0.0;
+  obj->value[11] = 0.0;
   obj->value[15] = 1.0;
   return obj;
 }
 
 CMatrix4* cmatrix4_setScale(CMatrix4* obj, double x, double y, double z) {
   obj->value[0] = x;
-  obj->value[1] = 0.0;
-  obj->value[2] = 0.0;
-  obj->value[3] = 0.0;
-
-  obj->value[4] = y;
-  obj->value[5] = 1.0;
-  obj->value[6] = 0.0;
-  obj->value[7] = 0.0;
-
+  obj->value[4] = 0.0;
   obj->value[8] = 0.0;
-  obj->value[9] = 0.0;
-  obj->value[10] = z;
-  obj->value[11] = 0.0;
-
   obj->value[12] = 0.0;
+
+  obj->value[1] = y;
+  obj->value[5] = 1.0;
+  obj->value[9] = 0.0;
   obj->value[13] = 0.0;
+
+  obj->value[2] = 0.0;
+  obj->value[6] = 0.0;
+  obj->value[10] = z;
   obj->value[14] = 0.0;
+
+  obj->value[3] = 0.0;
+  obj->value[7] = 0.0;
+  obj->value[11] = 0.0;
   obj->value[15] = 1.0;
   return obj;
 }
