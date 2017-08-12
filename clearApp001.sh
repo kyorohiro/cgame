c263d0cdaee58dedc769854a48c4e7d7de11446f
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-12 15:23:17 +0900

    ..

---
+++ clearApp001.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#!/bin/sh
+
+find ./app_001 -name "*.bc" | xargs rm
+find ./app_001 -name "*.o" | xargs rm
+find ./app_001 -name "*.js" | xargs rm
+find ./app_001 -name "*.html" | xargs rm
--- clearApp2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#!/bin/sh
-
-find ./app2 -name "*.bc" | xargs rm
-find ./app2 -name "*.o" | xargs rm
-find ./app2 -name "*.js" | xargs rm
-find ./app2 -name "*.html" | xargs rm
