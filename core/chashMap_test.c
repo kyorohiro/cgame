27aea146b37d93e77a39d3058579b64ec638a656
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-13 12:11:06 +0900

    ..

--- core/chashMap_test.c
+++ core/chashMap_test.c
@@ -13,12 +13,12 @@ void chashMap_test(){
 
 
   CString * key1 = initCString(newCString(mem),"k1");
-//  CString * key2 = initCString(newCString(mem),"k2");
+  CString * key2 = initCString(newCString(mem),"k2");
   CString * abc = initCString(newCString(mem),"abc");
-//  CString * def = initCString(newCString(mem),"abc");
+  CString * def = initCString(newCString(mem),"abc");
 
   //chashMap_put(map, cobject_downCounter((CObject*)key1), cobject_downCounter((CObject*)abc));
-//  chashMap_put(map, cobject_downCounter((CObject*)key2), cobject_downCounter((CObject*)def));
+  chashMap_put(map, cobject_downCounter((CObject*)key2), cobject_downCounter((CObject*)def));
   chashMap_put(map, (CObject*)key1, (CObject*)abc);
   releaseCObject((CObject*)map);
   //printf(" >>>>>  %d %d\n", ((CObject*)key1)->reference, ((CObject*)key1)->mode);
