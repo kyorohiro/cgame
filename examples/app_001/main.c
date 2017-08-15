0be541478e38f73c01ec1c4ee23a38883f8aa05f
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-15 21:33:08 +0900

    ..

--- examples/app_001/main.c
+++ examples/app_001/main.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include "app/capp.h"
+#include "core/cstring.h"
+#include "core/cmemory.h"
 
+
+void _testOnDisplay(CObject* context, CObject* args) {
+  CApp* appObj = getCApp();
+  capp_postRedisplay(appObj);
+  capp_flushBuffers(appObj);
+}
 
 int main(int argc, char** argv) {
   printf("test");
   CApp* appObj = getCApp();
+  CString *context = initCString(newCString(getCMemory()), "dummy");
+  capp_addDisplayEventListener(appObj, (CObject*)context, _testOnDisplay);
   capp_run(appObj);
+
   return 0;
 }
