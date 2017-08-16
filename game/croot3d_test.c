3106b058f5fddcf86a440999db4845e4f214cfaa
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-16 10:20:12 +0900

    ..

--- game/croot3d_test.c
+++ game/croot3d_test.c
@@ -4,17 +4,14 @@
 #include "core/clinkedList.h"
 #include "core/cbytes.h"
 #include "cobject3d.h"
+#include "croot3d.h"
 
 void croot3d_test(){
   printf("croot3d_test\r\n");
   int passed = 1;
   CMemory *mem = initCMemory(newCMemory());
-  CObject3D *bye = initCObject3D(newCObject3D(mem));
+  CRoot3D *bye = initCRoot3D(newCRoot3D(mem));
 
-  {
-    CObject *o = cobject_downCounter((CObject*)initCObject3D(newCObject3D(mem)));
-    cobject3d_addNode(bye, (CObject3D*)o);
-  }
   releaseCObject((CObject*)bye);
   if(mem->callocCounter != mem->freeCounter) {
     printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
