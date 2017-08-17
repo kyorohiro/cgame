4226f13acc0314620fd155843031b27524d8cde2
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-17 18:26:00 +0900

    ..

--- matrix/cvector3.c
+++ matrix/cvector3.c
@@ -16,7 +16,7 @@ CVector3* newCVector3(CMemory* cmemory) {
   return ret;
 }
 
-CVector3* initCVector3(CVector3* obj, CMatrixVertexType v0, CMatrixVertexType v1, CMatrixVertexType v2) {
+CVector3* initCVector3(CVector3* obj, CMatrixValueType v0, CMatrixValueType v1, CMatrixValueType v2) {
   initCObject((CObject*)obj, KVECTOR3_NAME);
   obj->value[0] = v0;
   obj->value[1] = v1;
@@ -33,29 +33,29 @@ CVector3* cvector3_crossProduct(CVector3* obj, CVector3* arg, CVector3* out) {
 }
 
 CVector3Raw* cvector3raw_crossProduct(CVector3Raw *obj, CVector3Raw *arg, CVector3Raw *out) {
-  CMatrixVertexType v0 = *obj[1] * *arg[2] - *obj[2] * *arg[1];
-  CMatrixVertexType v1 = *obj[2] * *arg[0] - *obj[0] * *arg[2];
-  CMatrixVertexType v2 = *obj[0] * *arg[1] - *obj[1] * *arg[0];
+  CMatrixValueType v0 = *obj[1] * *arg[2] - *obj[2] * *arg[1];
+  CMatrixValueType v1 = *obj[2] * *arg[0] - *obj[0] * *arg[2];
+  CMatrixValueType v2 = *obj[0] * *arg[1] - *obj[1] * *arg[0];
   *out[0] = v0;
   *out[1] = v1;
   *out[2] = v2;
   return out;
 }
 
-CMatrixVertexType cvector3_dotProduct(CVector3* obj, CVector3* arg) {
+CMatrixValueType cvector3_dotProduct(CVector3* obj, CVector3* arg) {
   return cvector3raw_dotProduct(&obj->value, &arg->value);
 }
 
-CMatrixVertexType cvector3raw_dotProduct(CVector3Raw* obj, CVector3Raw* arg) {
-  double sum;
+CMatrixValueType cvector3raw_dotProduct(CVector3Raw* obj, CVector3Raw* arg) {
+  CMatrixValueType sum;
   sum  = *obj[0] * *arg[0];
   sum += *obj[1] * *arg[1];
   sum += *obj[2] * *arg[2];
   return sum;
 }
 
-CMatrixVertexType cvector3_normalize(CVector3* obj) {
-  double v =
+CMatrixValueType cvector3_normalize(CVector3* obj) {
+  CMatrixValueType v =
          (obj->value[0]*obj->value[0]) +
          (obj->value[1]*obj->value[1]) +
          (obj->value[2]*obj->value[2]);
--- matrix/cvector3.h
+++ matrix/cvector3.h
@@ -6,7 +6,7 @@
 #include "cmatrix_inner.h"
 #define KVECTOR3_NAME "ve4"
 
-typedef CMatrixVertexType CVector3Raw[3];
+typedef CMatrixValueType CVector3Raw[3];
 
 typedef struct {
   CObject parent;
@@ -14,14 +14,14 @@ typedef struct {
 } CVector3;
 
 CVector3* newCVector3(CMemory*);
-CVector3* initCVector3(CVector3*, CMatrixVertexType v0, CMatrixVertexType v1, CMatrixVertexType v2);
+CVector3* initCVector3(CVector3*, CMatrixValueType v0, CMatrixValueType v1, CMatrixValueType v2);
 
 CVector3* cvector3_crossProduct(CVector3* obj, CVector3* arg, CVector3* out);
-CMatrixVertexType cvector3_dotProduct(CVector3* obj, CVector3* arg);
-CMatrixVertexType cvector3_normalize(CVector3* obj);
+CMatrixValueType cvector3_dotProduct(CVector3* obj, CVector3* arg);
+CMatrixValueType cvector3_normalize(CVector3* obj);
 CVector3* cvector3_add(CVector3* obj, CVector3* arg, CVector3* out);
 CVector3* cvector3_sub(CVector3* obj, CVector3* arg, CVector3* out);
 
 CVector3Raw* cvector3raw_crossProduct(CVector3Raw *obj, CVector3Raw *arg, CVector3Raw *out);
-CMatrixVertexType cvector3raw_dotProduct(CVector3Raw* obj, CVector3Raw* arg);
+CMatrixValueType cvector3raw_dotProduct(CVector3Raw* obj, CVector3Raw* arg);
 #endif
