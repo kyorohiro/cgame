497930d3df69ed916e2435daa9e0c75b9a507650
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-18 20:02:47 +0900

    ..

---
+++ matrix/cmatrix3.c
@@ -0,0 +1,292 @@
+#include "cmatrix3.h"
+#include "cvector3.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void _freeMatrix(void* obj);
+
+CMatrix3* newCMatrix3(CMemory* cmemory) {
+  CMatrix3 *ret = (CMatrix3*)cmemory_calloc(cmemory, 1, sizeof(CMatrix3));
+  ret->parent.funcFree = _freeMatrix;
+  ret->parent.cmemory = cmemory;
+  return ret;
+}
+
+CMatrix3* initCMatrix3(CMatrix3* obj) {
+  initCObject((CObject*)obj, KMATRIX_NAME);
+  return obj;
+}
+
+void _freeMatrix(void* tmp) {
+  CMatrix3* obj = tmp;
+  freeCObject(obj);
+}
+
+CMatrix3* cmatrix3_add(CMatrix3* obj, CMatrix3* arg, CMatrix3* out) {
+  int i=0;
+  if(out ==NULL) {
+    out = initCMatrix3(newCMatrix3(obj->parent.cmemory));
+  }
+  cmatrix3raw_add(obj->value, arg->value, out->value);
+  return out;
+}
+
+CMatrix3* cmatrix3_sub(CMatrix3* obj, CMatrix3* arg, CMatrix3* out) {
+  int i=0;
+  if(out ==NULL) {
+    out = initCMatrix3(newCMatrix3(obj->parent.cmemory));
+  }
+  cmatrix3raw_sub(obj->value, arg->value, out->value);
+  return out;
+}
+
+CMatrix3* cmatrix3_mul(CMatrix3* obj, CMatrix3* arg, CMatrix3* out) {
+  if(out ==NULL) {
+    out = initCMatrix3(newCMatrix3(obj->parent.cmemory));
+  }
+
+  cmatrix3raw_mul(obj->value, arg->value, out->value);
+
+  return out;
+}
+
+CVector3* cmatrix3_mulCVector3(CMatrix3* obj, CVector3* arg, CVector3* out) {
+  if(out == NULL) {
+    out = initCVector3(newCVector3(obj->parent.cmemory), 0.0, 0.0, 0.0);
+  }
+  cmatrix3raw_mulVector3Raw(obj->value, arg->value, out->value);
+  return out;
+}
+
+CMatrix3* cmatrix3_setIdentity(CMatrix3* obj) {
+  cmatrix3raw_setIdentity(obj->value);
+  return obj;
+}
+
+CMatrix3* cmatrix3_setValues(CMatrix3* obj,
+              CMatrixValueType a11, CMatrixValueType a12, CMatrixValueType a13,
+              CMatrixValueType a21, CMatrixValueType a22, CMatrixValueType a23,
+              CMatrixValueType a31, CMatrixValueType a32, CMatrixValueType a33
+            ) {
+  cmatrix3raw_setValues(obj->value,
+    a11, a12, a13,
+    a21, a22, a23,
+    a31, a32, a33);
+  return obj;
+}
+
+CMatrixValueType cmatrix3_determinant(CMatrix3* obj) {
+  return cmatrix3raw_determinant(obj->value);
+}
+
+//
+//http://mathworld.wolfram.com/MatrixInverse.html
+CMatrix3* cmatrix3_inverse(CMatrix3* obj, CMatrix3* outInverse, CMatrixValueType *outDeterminant) {
+  if(outInverse ==NULL) {
+    outInverse = initCMatrix3(newCMatrix3(obj->parent.cmemory));
+  }
+  cmatrix3raw_inverse(obj->value, outInverse->value, outDeterminant);
+  return outInverse;
+}
+
+CMatrix3* cmatrix3_transpose(CMatrix3* obj, CMatrix3* out) {
+  if(out == NULL) {
+    out = initCMatrix3(newCMatrix3(obj->parent.cmemory));
+  }
+  cmatrix3raw_transpose(obj->value, out->value);
+  return out;
+}
+
+CMatrixValueType cmatrix3_getValue(CMatrix3* obj, int row, int col) {
+  return cmatrix3raw_getValue(obj->value, row, col);
+}
+
+void cmatrix3_show(CMatrix3* obj){
+  cmatrix3raw_show(obj->value);
+}
+
+//
+// raw
+//
+CMatrix3RawRef cmatrix3raw_setIdentity(CMatrix3RawRef obj) {
+  obj[0] = 1.0;
+  obj[3] = 0.0;
+  obj[6] = 0.0;
+
+  obj[1] = 0.0;
+  obj[4] = 1.0;
+  obj[7] = 0.0;
+
+  obj[2] = 0.0;
+  obj[5] = 0.0;
+  obj[8] = 1.0;
+  return obj;
+}
+
+CMatrix3RawRef cmatrix3raw_setValues(CMatrix3RawRef obj,
+              CMatrixValueType a11, CMatrixValueType a12, CMatrixValueType a13,
+              CMatrixValueType a21, CMatrixValueType a22, CMatrixValueType a23,
+              CMatrixValueType a31, CMatrixValueType a32, CMatrixValueType a33) {
+  obj[0] = a11;
+  obj[3] = a12;
+  obj[6] = a13;
+
+  obj[1] = a21;
+  obj[4] = a22;
+  obj[7] = a23;
+
+  obj[2] = a31;
+  obj[5] = a32;
+  obj[8] = a33;
+
+  return obj;
+}
+
+CMatrix3RawRef cmatrix3raw_copy(CMatrix3RawRef obj, CMatrix3RawRef out) {
+  for(int i=0;i<16;i++) {
+    out[i] = obj[i];
+  }
+  return out;
+}
+
+CMatrix3RawRef cmatrix3raw_add(CMatrix3RawRef obj, CMatrix3RawRef arg, CMatrix3RawRef out) {
+  out[0]  = obj[0]  + arg[0];
+  out[1]  = obj[1]  + arg[1];
+  out[2]  = obj[2]  + arg[2];
+  out[3]  = obj[3]  + arg[3];
+  out[4]  = obj[4]  + arg[4];
+  out[5]  = obj[5]  + arg[5];
+  out[6]  = obj[6]  + arg[6];
+  out[7]  = obj[7]  + arg[7];
+  out[8]  = obj[8]  + arg[8];
+  return out;
+}
+
+CMatrix3RawRef cmatrix3raw_sub(CMatrix3RawRef obj, CMatrix3RawRef arg, CMatrix3RawRef out) {
+  out[0]  = obj[0]  - arg[0];
+  out[1]  = obj[1]  - arg[1];
+  out[2]  = obj[2]  - arg[2];
+  out[3]  = obj[3]  - arg[3];
+  out[4]  = obj[4]  - arg[4];
+  out[5]  = obj[5]  - arg[5];
+  out[6]  = obj[6]  - arg[6];
+  out[7]  = obj[7]  - arg[7];
+  out[8]  = obj[8]  - arg[8];
+
+  return out;
+}
+
+CMatrix3RawRef cmatrix3raw_mul(CMatrix3RawRef obj, CMatrix3RawRef arg, CMatrix3RawRef out) {
+  double o[16];
+  for(int i=0;i<3;i++) {
+    for(int j=0;j<3;j++) {
+      o[i+3*j]  = obj[i+0] * arg[0+3*j];
+      o[i+3*j] += obj[i+3] * arg[1+3*j];
+      o[i+3*j] += obj[i+6] * arg[2+3*j];
+    }
+  }
+
+  for(int k=0;k<9;k++) {
+    out[k] = o[k];
+  }
+
+  return out;
+}
+
+CVector3RawRef cmatrix3raw_mulVector3Raw(CMatrix3RawRef obj, CVector3RawRef arg, CVector3RawRef out) {
+  CMatrixValueType v0 = obj[0]*arg[0] + obj[3]*arg[1] + obj[6]*arg[2];
+  CMatrixValueType v1 = obj[1]*arg[0] + obj[4]*arg[1] + obj[7]*arg[2];
+  CMatrixValueType v2 = obj[2]*arg[0] + obj[5]*arg[1] + obj[8]*arg[2];
+  out[0] = v0;
+  out[1] = v1;
+  out[2] = v2;
+  return out;
+}
+
+CMatrix3RawRef cmatrix3raw_transpose(CMatrix3RawRef obj, CMatrix3RawRef out) {
+
+  CMatrixValueType a11 = obj[0];
+  CMatrixValueType a12 = obj[1];
+  CMatrixValueType a13 = obj[2];
+  CMatrixValueType a21 = obj[3];
+  CMatrixValueType a22 = obj[4];
+  CMatrixValueType a23 = obj[5];
+  CMatrixValueType a31 = obj[6];
+  CMatrixValueType a32 = obj[7];
+  CMatrixValueType a33 = obj[8];
+
+  out[0] = a11;
+  out[3] = a12;
+  out[6] = a13;
+
+  out[1] = a21;
+  out[4] = a22;
+  out[7] = a23;
+
+  out[2] = a31;
+  out[5] = a32;
+  out[8] = a33;
+
+  return out;
+}
+
+
+//
+// 0 3 6
+// 1 4 7
+// 2 5 8
+CMatrixValueType cmatrix3raw_determinant(CMatrix3RawRef raw) {
+  CMatrixValueType a0 = raw[0] *(raw[4] * raw[8] - raw[5] * raw[7]);
+  CMatrixValueType a1 = raw[1] *(raw[3] * raw[8] - raw[5] * raw[6]);
+  CMatrixValueType a2 = raw[2] *(raw[3] * raw[7] - raw[4] * raw[6]);
+
+  return a0 - a1 + a2;
+}
+
+// 0 3 6
+// 1 4 7
+// 2 5 8
+CMatrix3RawRef cmatrix3raw_inverse(CMatrix3RawRef raw, CMatrix3RawRef outInverse, CMatrixValueType *outDeterminant) {
+  CMatrixValueType det = cmatrix3raw_determinant(raw);
+  if(outInverse != NULL) {
+    *outInverse = det;
+  }
+  if(det == 0) {
+    return outInverse;
+  }
+  CMatrixValueType i0 =(raw[4] * raw[8] - raw[5] * raw[7]);
+  CMatrixValueType i3 =-1*(raw[1] * raw[8] - raw[7] * raw[2]);
+  CMatrixValueType i6 =(raw[1] * raw[5] - raw[4] * raw[2]);
+  CMatrixValueType i1 =-1*(raw[3] * raw[8] - raw[6] * raw[5]);
+  CMatrixValueType i4 =(raw[0] * raw[8] - raw[2] * raw[6]);
+  CMatrixValueType i7 =-1*(raw[0] * raw[5] - raw[3] * raw[2]);
+  CMatrixValueType i2 =(raw[3] * raw[7] - raw[6] * raw[4]);
+  CMatrixValueType i5 =-1*(raw[0] * raw[7] - raw[6] * raw[1]);
+  CMatrixValueType i8 =(raw[0] * raw[4] - raw[3] * raw[1]);
+
+  outInverse[0] = i0;
+  outInverse[1] = i3;
+  outInverse[2] = i6;
+  outInverse[3] = i1;
+  outInverse[4] = i4;
+  outInverse[5] = i7;
+  outInverse[6] = i2;
+  outInverse[7] = i5;
+  outInverse[8] = i8;
+
+  return outInverse;
+}
+
+CMatrixValueType cmatrix3raw_getValue(CMatrix3RawRef obj, int row, int col) {
+  return obj[row+3*col];
+}
+
+void cmatrix3raw_show(CMatrix3RawRef obj) {
+  for(int i=0;i<3;i++) {
+    printf("%f %f %f \r\n",
+      cmatrix3raw_getValue(obj, i, 0),
+      cmatrix3raw_getValue(obj, i, 1),
+      cmatrix3raw_getValue(obj, i, 2));
+  }
+}
---
+++ matrix/cmatrix3.h
@@ -0,0 +1,69 @@
+
+#ifndef __H_GL_MATRIX3_
+#define __H_GL_MATRIX3_
+
+#include "core/cobject.h"
+#include "core/cmemory.h"
+#include "cvector3.h"
+#include "cmatrix_inner.h"
+
+#define KMATRIX_NAME "kma"
+
+typedef CMatrixValueType CMatrix3Raw[9];
+typedef CMatrixValueType *CMatrix3RawRef;
+
+typedef struct {
+  CObject parent;
+  CMatrix3Raw value;
+} CMatrix3;
+
+CMatrix3* newCMatrix3(CMemory*);
+CMatrix3* initCMatrix3(CMatrix3*);
+CMatrix3* cmatrix3_setIdentity(CMatrix3* obj);
+CMatrix3* cmatrix3_setValues(CMatrix3* obj,
+              CMatrixValueType a11, CMatrixValueType a12, CMatrixValueType a13,
+              CMatrixValueType a21, CMatrixValueType a22, CMatrixValueType a23,
+              CMatrixValueType a31, CMatrixValueType a32, CMatrixValueType a33);
+
+CMatrix3* cmatrix3_add(CMatrix3* obj, CMatrix3* arg, CMatrix3* out);
+CMatrix3* cmatrix3_sub(CMatrix3* obj, CMatrix3* arg, CMatrix3* out);
+CMatrix3* cmatrix3_mul(CMatrix3* obj, CMatrix3* arg, CMatrix3* out);
+CVector3* cmatrix3_mulCVector3(CMatrix3* obj, CVector3* arg, CVector3* out);
+CMatrix3* cmatrix3_transpose(CMatrix3* obj, CMatrix3* out);
+CMatrixValueType cmatrix3_determinant(CMatrix3* obj);
+CMatrix3* cmatrix3_inverse(CMatrix3* obj, CMatrix3* outInverse, CMatrixValueType *outDeterminant);
+
+//     col 0 1 2 3
+// row
+//   0
+//   1
+//   2
+//   3
+CMatrixValueType cmatrix3_getValue(CMatrix3* obj, int row, int col);
+void cmatrix3_show(CMatrix3* obj);
+
+
+//
+//
+// RAW
+//
+//
+CMatrix3RawRef cmatrix3raw_setIdentity(CMatrix3RawRef obj);
+
+CMatrix3RawRef cmatrix3raw_setValues(CMatrix3RawRef obj,
+              CMatrixValueType a11, CMatrixValueType a12, CMatrixValueType a13,
+              CMatrixValueType a21, CMatrixValueType a22, CMatrixValueType a23,
+              CMatrixValueType a31, CMatrixValueType a32, CMatrixValueType a33
+            );
+CMatrix3RawRef cmatrix3raw_copy(CMatrix3RawRef obj, CMatrix3RawRef out);
+CMatrix3RawRef cmatrix3raw_add(CMatrix3RawRef obj, CMatrix3RawRef arg, CMatrix3RawRef out);
+CMatrix3RawRef cmatrix3raw_sub(CMatrix3RawRef obj, CMatrix3RawRef arg, CMatrix3RawRef out);
+CMatrix3RawRef cmatrix3raw_mul(CMatrix3RawRef obj, CMatrix3RawRef arg, CMatrix3RawRef out);
+CVector3RawRef cmatrix3raw_mulVector3Raw(CMatrix3RawRef obj, CVector3RawRef arg, CVector3RawRef out);
+CMatrix3RawRef cmatrix3raw_transpose(CMatrix3RawRef obj, CMatrix3RawRef out);
+CMatrixValueType cmatrix3raw_determinant(CMatrix3RawRef obj);
+CMatrix3RawRef cmatrix3raw_inverse(CMatrix3RawRef obj, CMatrix3RawRef outInverse, CMatrixValueType *outDeterminant);
+
+CMatrixValueType cmatrix3raw_getValue(CMatrix3RawRef obj, int row, int col);
+void cmatrix3raw_show(CMatrix3RawRef obj);
+#endif
