db082d9eed261d0a06f5af529ae7eeba86ae00d3
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-19 22:04:18 +0900

    ..

--- matrix/cmatrix2_test.c
+++ matrix/cmatrix2_test.c
@@ -17,7 +17,6 @@ void cmatrix2_test() {
       2, 1);
 
   CMatrix2 *out = cmatrix2_add(mat1, mat2, NULL);
-
   for(int i=0;i<2;i++) {
     for(int j=0;j<2;j++) {
       if(cmatrix2_getValue(out, i, j) != 5) {
@@ -27,6 +26,14 @@ void cmatrix2_test() {
       }
     }
   }
+
+  cmatrix2_sub(mat1, mat2, out);
+  if(cmatrix2_getValue(out, 0, 0) != -3 || cmatrix2_getValue(out, 0, 1) != -1 ||
+     cmatrix2_getValue(out, 1, 0) !=1 || cmatrix2_getValue(out, 1, 1) != 3) {
+       printf("  NG : sub\r\n");
+       cmatrix2_show(out);
+       passed = 0;
+     }
   //
   //
   releaseCObject((CObject*)mat1);
