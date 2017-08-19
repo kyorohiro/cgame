c253af95ef32e48df3434ec309ed28a50364f348
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-19 21:43:45 +0900

    ..

--- matrix/cvector4_test.c
+++ matrix/cvector4_test.c
@@ -39,11 +39,50 @@ void cvector4_test_add() {
     passed = 0;
   }
 
+  //
+  // 1 2 3 4
+  // 5 6 7 8
+  // 5 12 21 32
+  // 17 53 ->70
+  CMatrixValueType x = cvector4_dotProduct(vec1, vec2);
+  if(((int)x) != 70) {
+    printf("  NG : failed to dotProduct %f \r\n", x);
+    passed = 0;
+  }
+
+  //
+  //
+  // 1 2 3 4,
+  // 5 6 7 8,
+  // 4 4 4 4
+  // 4*4*4 = 16*4 = 64
+  // sqrt 64> 8
+  //
+  CMatrixValueType y = cvector4_distance(vec1, vec2);
+  if(y != 8.0) {
+    printf("  NG : failed to distance %f \r\n", y);
+    passed = 0;
+  }
+
+  //
+  //
+  // 1 2 3 4,
+  // 1 4 9 16
+  // 5 25 > 30
+  // sqrt 30
+  //
+  CMatrixValueType z = cvector4_length(vec1);
+  if((int)(1000*z) != 5477) {
+    printf("  NG : failed to distance %f \r\n", z);
+    passed = 0;
+  }
+
+
+
   releaseCObject((CObject*)vec1);
   releaseCObject((CObject*)vec2);
   releaseCObject((CObject*)vec3);
   releaseCObject((CObject*)vec5);
-
   if(mem->callocCounter != mem->freeCounter) {
     printf("  NG : %d == %d\r\n", mem->callocCounter, mem->freeCounter);
     passed = 0;
