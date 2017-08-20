eae53ca044111b3ea21447e538fee0065223fafa
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-20 11:38:44 +0900

    ..

--- matrix/cmatrix2_test.c
+++ matrix/cmatrix2_test.c
@@ -57,7 +57,6 @@ void cmatrix2_test() {
   // 1*1+2*2 > 5
   // 1*3+4*2 > 11
   CVector2 *vec1 = initCVector2(newCVector2(mem), 1.0, 2.0);
-
   CVector2 *vecO = cmatrix2_mulCVector2(mat1, vec1, NULL);
   if(vecO->value[0] != 5 || vecO->value[1] != 11) {
        printf("  NG : mulVec\r\n");
@@ -65,7 +64,30 @@ void cmatrix2_test() {
        cvector2_show(vec1);
        cvector2_show(vecO);
        passed = 0;
-     }
+  }
+
+  //
+  // 1 2 > 1 3
+  // 3 4 > 2 4
+  //
+  cmatrix2_transpose(mat1, out);
+  if(cmatrix2_getValue(out, 0, 0) != 1 || cmatrix2_getValue(out, 0, 1) != 3 ||
+     cmatrix2_getValue(out, 1, 0) != 2 || cmatrix2_getValue(out, 1, 1) != 4) {
+       printf("  NG : transpose\r\n");
+       cmatrix2_show(out);
+       passed = 0;
+  }
+
+  //
+  // 1 2
+  // 3 4 > 1*4 - 2*3 > -2
+  CMatrixValueType v = cmatrix2_determinant(mat1);
+  if(v!= -2) {
+       printf("  NG : determinant %f\r\n", v);
+       cmatrix2_show(out);
+       passed = 0;
+  }
+
   releaseCObject((CObject*)mat1);
   releaseCObject((CObject*)mat2);
   releaseCObject((CObject*)out);
