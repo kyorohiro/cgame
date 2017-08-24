fba3e134d5bb6a7cbce0e0af0b0ba54601666000
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-24 23:27:03 +0900

    ..

--- matrix/cmatrix_trans.c
+++ matrix/cmatrix_trans.c
@@ -137,3 +137,126 @@ CMatrix4* cmatrix4_setScale(CMatrix4* obj, double x, double y, double z) {
   obj->value[15] = 1.0;
   return obj;
 }
+
+
+CMatrix4RawRef cmatrix4raw_setRotationX(CMatrix4RawRef obj, double radians) {
+  double c = cos(radians);
+  double s = sin(radians);
+  obj[0] = 1;
+  obj[4] = 0.0;
+  obj[8] = 0.0;
+  obj[12] = 0.0;
+
+  obj[1] = 0.0;
+  obj[5] = c;
+  obj[9] = -s;
+  obj[13] = 0.0;
+
+  obj[2] = 0.0;
+  obj[6] =  s;
+  obj[10] = c;
+  obj[14] = 0.0;
+
+  obj[3] = 0.0;
+  obj[7] = 0.0;
+  obj[11] = 0.0;
+  obj[15] = 1.0;
+  return obj;
+}
+
+CMatrix4RawRef cmatrix4raw_setRotationY(CMatrix4RawRef obj, double radians) {
+  double c = cos(radians);
+  double s = sin(radians);
+  obj[0] = c;
+  obj[4] = 0.0;
+  obj[8] = s;
+  obj[12] = 0.0;
+
+  obj[1] = 0.0;
+  obj[5] = 1.0;
+  obj[9] = 0.0;
+  obj[13] = 0.0;
+
+  obj[2] =  -s;
+  obj[6] = 0.0;
+  obj[10] = c;
+  obj[14] = 0.0;
+
+  obj[3] = 0.0;
+  obj[7] = 0.0;
+  obj[11] = 0.0;
+  obj[15] = 1.0;
+
+  return obj;
+}
+
+CMatrix4RawRef cmatrix4raw_setRotationZ(CMatrix4RawRef obj, double radians) {
+  double c = cos(radians);
+  double s = sin(radians);
+  obj[0] = c;
+  obj[4] = -s;
+  obj[8] = 0.0;
+  obj[12] = 0.0;
+
+  obj[1] = s;
+  obj[5] = c;
+  obj[9] = 0.0;
+  obj[13] = 0.0;
+
+  obj[2] = 0.0;
+  obj[6] = 0.0;
+  obj[10] = 1.0;
+  obj[14] = 0.0;
+
+  obj[3] = 0.0;
+  obj[7] = 0.0;
+  obj[11] = 0.0;
+  obj[15] = 1.0;
+  return obj;
+}
+
+CMatrix4RawRef cmatrix4raw_setTranslation(CMatrix4RawRef obj, double x, double y, double z) {
+  obj[0] = 1.0;
+  obj[4] = 0.0;
+  obj[8] = 0.0;
+  obj[12] = x;
+
+  obj[1] = 0.0;
+  obj[5] = 1.0;
+  obj[9] = 0.0;
+  obj[13] = y;
+
+  obj[2] = 0.0;
+  obj[6] = 0.0;
+  obj[10] = 1.0;
+  obj[14] = z;
+
+  obj[3] = 0.0;
+  obj[7] = 0.0;
+  obj[11] = 0.0;
+  obj[15] = 1.0;
+  return obj;
+}
+
+CMatrix4RawRef cmatrix4raw_setScale(CMatrix4RawRef obj, double x, double y, double z) {
+  obj[0] = x;
+  obj[4] = 0.0;
+  obj[8] = 0.0;
+  obj[12] = 0.0;
+
+  obj[1] = 0.0;
+  obj[5] = y;
+  obj[9] = 0.0;
+  obj[13] = 0.0;
+
+  obj[2] = 0.0;
+  obj[6] = 0.0;
+  obj[10] = z;
+  obj[14] = 0.0;
+
+  obj[3] = 0.0;
+  obj[7] = 0.0;
+  obj[11] = 0.0;
+  obj[15] = 1.0;
+  return obj;
+}
--- matrix/cmatrix_trans.h
+++ matrix/cmatrix_trans.h
@@ -3,6 +3,7 @@
 
 #include "core/cobject.h"
 #include "core/cmemory.h"
+#include "cmatrix_inner.h"
 #include "cmatrix4.h"
 #include "cvector3.h"
 
@@ -11,5 +12,13 @@ CMatrix4* cmatrix4_setRotationY(CMatrix4* obj, double radians);
 CMatrix4* cmatrix4_setRotationZ(CMatrix4* obj, double radians);
 CMatrix4* cmatrix4_setTranslation(CMatrix4* obj, double x, double y, double z);
 CMatrix4* cmatrix4_setScale(CMatrix4* obj, double x, double y, double z);
+
+
+
+CMatrix4RawRef cmatrix4raw_setRotationX(CMatrix4RawRef obj, double radians);
+CMatrix4RawRef cmatrix4raw_setRotationY(CMatrix4RawRef obj, double radians);
+CMatrix4RawRef cmatrix4raw_setRotationZ(CMatrix4RawRef obj, double radians);
+CMatrix4RawRef cmatrix4raw_setTranslation(CMatrix4RawRef obj, double x, double y, double z);
+CMatrix4RawRef cmatrix4raw_setScale(CMatrix4RawRef obj, double x, double y, double z);
 
 #endif
