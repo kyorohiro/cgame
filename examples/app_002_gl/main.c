39962d768e108314efdb21e157d3257ec965169a
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-08-30 04:43:28 +0900

    ..

---
+++ examples/app_002_gl/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "app/capp.h"
+#include "core/cstring.h"
+#include "core/cmemory.h"
+
+int fps;
+void _testOnDisplay(CObject* context, CObject* args) {
+  
+  CApp* appObj = getCApp();
+  capp_postRedisplay(appObj);
+  if(fps != appObj->fps) {
+    fps = appObj->fps;
+    printf("fps:%d;\r\n",fps);
+  }
+  //capp_flushBuffers(appObj);
+}
+
+int main(int argc, char** argv) {
+  printf("capp sample");
+  CApp* appObj = getCApp();
+  CString *context = initCString(newCString(getCMemory()), "dummy");
+  capp_addDisplayEventListener(appObj, (CObject*)context, _testOnDisplay);
+  capp_run(appObj);
+  return 0;
+}
