6233f7e3b33e77e2d3e54be1176f63732727f0fd
kyorohiro <kyorohiro@kyorohironoMac-mini.local>
2017-09-05 04:26:47 +0900

    ..

---
+++ game/csprite.h
@@ -0,0 +1,25 @@
+#ifndef _H_3DPRIMITIVE
+#define _H_3DPRIMITIVE
+
+
+#include "core/cobject.h"
+#include "core/cmemory.h"
+#include "matrix/cmatrix4.h"
+#include "core/cbytes.h"
+#include "core/clinkedList.h"
+#include "matrix/cmatrix4.h"
+#include "cobject3d.h"
+#include "cprimitive3d.h"
+
+#define SPRITE3D_NAME "sp3"
+
+//
+//
+typedef struct {
+  CPrimitive3D parent;
+} CSprite;
+
+CSprite* newCSprite(CMemory* mem);
+CSprite* initCSpriteFromSingleImage(CSprite*, char* imgPath);
+
+#endif
