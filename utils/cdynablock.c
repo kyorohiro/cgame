943c07416e4200f9a3326e57cd38c26d5ae18b41
kyorohiro <kyorohiro@gmail.com>
2017-09-12 05:25:07 +0900

    ..

--- utils/cdynablock.c
+++ utils/cdynablock.c
@@ -10,8 +10,9 @@ CDynaBlock* newCDynaBlock(CMemory* cmemory) {
   CDynaBlock* ret = cmemory_calloc(cmemory, 1, sizeof(CDynaBlock));
   ret->parent.cmemory = cmemory;
   ret->parent.funcFree = _freeCDynaBlock;
+  return ret;
 }
 
-CDynaBlock* initCDynaBlock(CDynaBlock* obj) {
+CDynaBlock* initCDynaBlock(CDynaBlock* obj, int w, int h, int maxOfSpace) {
   return obj;
 }
