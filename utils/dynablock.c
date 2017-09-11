3629ff60fcad9d141224f963d9258d1a87cb118b
kyorohiro <kyorohiro@gmail.com>
2017-09-12 03:47:35 +0900

    ..

--- utils/dynablock.c
+++ utils/dynablock.c
@@ -19,6 +19,7 @@ int numOfSpace = 0;
 DySpace spaces[10];
 int dyWidth = 100;
 int dyHeight = 100;
+int MIN = 1;
 
 void initDySpace(){
   spaces[0].x = 0;
@@ -35,7 +36,7 @@ int findSpace(int w, int h, Block* out) {
       if(tmp == NULL) {
         tmp = &spaces[i];
       }
-      if(spaces[i].y > tmp[i].y) {
+      if(spaces[i].y < tmp[i].y) {
         tmp = &spaces[i];
       }
     }
@@ -51,8 +52,75 @@ int findSpace(int w, int h, Block* out) {
   }
 }
 
-void search() {
 
+void updateIndex(int x, int y, int w, int opt) {
+  int isU = 0;
+  for(int i=0;i<numOfSpace;i++) {
+    if(spaces[i].y <= y-opt) {
+      if(spaces[i].y == y) {
+        isU = 1;
+      }
+      if(spaces[i].x <= x) {
+        if(x+w <(spaces[i].x+spaces[i].w)) {
+          //printf("s1\r\n");
+          //   aa
+          //  bbbb   -> d  b
+          spaces[i].x = x+w;
+          spaces[i].w = spaces[i].w-w;
+          if(x-spaces[i].x > MIN){
+            spaces[numOfSpace].x = spaces[i].x;
+            spaces[numOfSpace].y = spaces[i].y;
+            spaces[numOfSpace].w = x-spaces[i].x;
+            spaces[numOfSpace].h = dyHeight-y;
+            numOfSpace++;
+          }
+        } else {
+          //printf("s2\r\n");
+          //   aa
+          //  bb    ->  b
+          spaces[i].w = x-spaces[i].x;
+        }
+      } else {
+        if((spaces[i].x+spaces[i].w)< (x+w)) {
+          //printf("s3\r\n");
+          //  aaaa
+          //   bb     ->  0
+          spaces[i].x = spaces[i].y = spaces[i].w = spaces[i].h = 0;
+        } else {
+          //printf("s4\r\n");
+          //  aa
+          //   bb   --> b
+          spaces[i].x = x+w;
+        }
+      }
+    } else {
+      isU = 1;
+    }
+  }
+  if(isU == 0) {
+    printf("u1\r\n");
+    spaces[numOfSpace].x = x;
+    spaces[numOfSpace].y = y;
+    spaces[numOfSpace].w = dyWidth;
+    spaces[numOfSpace].h = dyHeight-y;
+    numOfSpace++;
+  }
+}
+
+void updateIndexBox(Block *b) {
+  updateIndex(b->x, b->y, b->w, 0);
+  updateIndex(b->x, b->y+b->h, b->w, 1);
+}
+
+void showDyBlock() {
+  for(int i=0;i<numOfSpace;i++) {
+    printf(">> %d %d %d %d\r\n",
+      spaces[i].x,
+      spaces[i].y,
+      spaces[i].w,
+      spaces[i].h);
+  }
+  printf("\r\n");
 }
 
 int main(int argc, char** argv) {
@@ -60,8 +128,28 @@ int main(int argc, char** argv) {
   initDySpace();
   Block out;
   int ret;
-  ret = findSpace(10,10, &out);
+  ret = findSpace(45,45, &out);
+  updateIndexBox(&out);
   printf("xywh : %d %d %d %d \r\n", out.x, out.y, out.w, out.h);
   printf("r : %d \r\n\r\n", ret);
+  showDyBlock();
+
+  //
+  //
+  ret = findSpace(45,45, &out);
+  updateIndexBox(&out);
+
+  printf("xywh : %d %d %d %d \r\n", out.x, out.y, out.w, out.h);
+  printf("r : %d \r\n\r\n", ret);
+  showDyBlock();
+
+
+  //
+  ret = findSpace(45,45, &out);
+  updateIndexBox(&out);
+  printf("xywh : %d %d %d %d \r\n", out.x, out.y, out.w, out.h);
+  printf("r : %d \r\n\r\n", ret);
+  showDyBlock();
+
   return 0;
 }
