5dd7940b758819a973d2bf2b08871b8d0080c8a2
kyorohiro <kyorohiro@gmail.com>
2017-09-15 02:26:53 +0900

    ..

---
+++ examples/game_004_primitive/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "game/cgame.h"
+#include "game/cobject3d.h"
+#include "game/cprimitive3d.h"
+#include "game/ccamera3d.h"
+#include "matrix/cmatrix.h"
+#include "matrix/cmatrix4.h"
+#include "matrix/cmatrix_proj.h"
+
+#include "app/capp.h"
+#include "app/cimage.h"
+#include "app/cttf.h"
+#include "core/cobject.h"
+
+int main(int argc, char** argv) {
+  printf("call main 1\r\n");
+  CGame* gameObj = createCGame(400, 300);
+  CImageMgr* mgr = getCImageMgr();
+
+  //
+  //
+  CObject3D *square1 = (CObject3D*)createPrimitive3DAsSquare();
+  cmatrix4_setTranslation(cobject3d_getCMatrix4((CObject3D*)square1), 0.0, 0.0, 0.0);
+
+  //
+  //
+  CObject3D *root = cgame_getRoot(gameObj);
+  cobject3d_addNode(root, square1);
+  ccamera3d_update(cgame_getCamera(gameObj),
+      0.0, 0.0, 5.0,
+      0.0, 0.0, 0.0,
+      3.14*45.0*0.5/180.0, 400.0/300.0, 0.5, 100.0);
+
+  cgame_run(gameObj);
+  return 0;
+}
