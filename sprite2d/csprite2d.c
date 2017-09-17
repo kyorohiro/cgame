63b18e1d7d87dc150a83ff7c56fcdc1cfadc1822
kyorohiro <kyorohiro@gmail.com>
2017-09-17 14:19:43 +0900

    ..

--- sprite2d/csprite2d.c
+++ sprite2d/csprite2d.c
@@ -21,3 +21,44 @@ CSprite2D* initCSprite2D(CSprite2D* obj, int width, int height) {
 CSprite2D* createCSprite2D(int width, int height) {
   return initCSprite2D(newCSprite2D(getCMemory()), width, height);
 }
+
+CSprite2D* csprite2d_setXYZ(CSprite2D* obj, CMatrixVertexType x, CMatrixVertexType y, CMatrixVertexType z) {
+  obj->x = x;
+  obj->y = y;
+  obj->z = z;
+  return obj;
+}
+
+CSprite2D* csprite2d_setRXYZ(CSprite2D* obj, CMatrixVertexType rx, CMatrixVertexType ry, CMatrixVertexType rz) {
+  obj->rx = rx;
+  obj->ry = ry;
+  obj->rz = rz;
+  return obj;
+}
+
+CSprite2D* csprite2d_update(CSprite2D* obj) {
+  CObject3D* obj3d = (CObject3D*)obj;
+  CMatrix4Raw center;
+  CMatrix4Raw centerB;
+  CMatrix4Raw move;
+
+  CMatrix4Raw rotateX;
+  CMatrix4Raw rotateY;
+  CMatrix4Raw rotateZ;
+  cmatrix4raw_setTranslation(center, obj3d->centerX, obj3d->centerY, obj3d->centerZ);
+  cmatrix4raw_setRotationX(rotateX, obj->rx);
+  cmatrix4raw_setRotationY(rotateX, obj->ry);
+  cmatrix4raw_setRotationZ(rotateX, obj->rz);
+  cmatrix4raw_setTranslation(centerB, obj3d->centerX, obj3d->centerY, obj3d->centerZ);
+  cmatrix4raw_setTranslation(move, obj->x, obj->y, obj->z);
+
+  CMatrix4* mat = obj3d->mat;
+  cmatrix4raw_mul(center, mat->value, mat->value);
+  cmatrix4raw_mul(rotateX, mat->value, mat->value);
+  cmatrix4raw_mul(rotateY, mat->value, mat->value);
+  cmatrix4raw_mul(rotateZ, mat->value, mat->value);
+  cmatrix4raw_mul(centerB, mat->value, mat->value);
+  cmatrix4raw_mul(move, mat->value, mat->value);
+
+  return obj;
+}
--- sprite2d/csprite2d.h
+++ sprite2d/csprite2d.h
@@ -21,4 +21,7 @@ typedef struct {
 CSprite2D* newCSprite2D(CMemory* mem);
 CSprite2D* initCSprite2D(CSprite2D*, int width, int height);
 CSprite2D* createCSprite2D(int width, int height);
+CSprite2D* csprite2d_setXYZ(CSprite2D*, CMatrixVertexType x, CMatrixVertexType y, CMatrixVertexType z);
+CSprite2D* csprite2d_setRXYZ(CSprite2D*, CMatrixVertexType rx, CMatrixVertexType ry, CMatrixVertexType rz);
+CSprite2D* csprite2d_update(CSprite2D*);
 #endif
