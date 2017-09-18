d74d14db3f383ba54f4bb32193fd31851e002df6
kyorohiro <kyorohiro@gmail.com>
2017-09-18 23:20:30 +0900

    ..

---
+++ examples/sprite2d_002_text/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include "game/cgame.h"
+#include "game/cobject3d.h"
+#include "game/cprimitive3d.h"
+#include "game/ccamera3d.h"
+#include "matrix/cmatrix.h"
+#include "app/capp.h"
+#include "app/cttf.h"
+#include "core/cobject.h"
+#include "sprite2d/csprite2d.h"
+
+int i =(180+2)%360;
+int j =(30+2)%360;
+int fps;
+CObject3D* sprite001;
+
+void _onEnterFrame(CObject*  obj, CObject* cgame) {
+  i =(i+1)%360;
+/*
+  csprite2d_setXYZ((CSprite2D*)sprite001, -1.0, -1.0, 0.0);
+  csprite2d_setRXYZ((CSprite2D*)sprite001, 0.0, 0.0, 2*3.14*i/360.0);
+  csprite2d_update((CSprite2D*)sprite001);
+*/
+  CApp* appObj = getCApp();
+  if(fps != appObj->fps) {
+    fps = appObj->fps;
+    printf("#fps:%d;\r\n",fps);
+  }
+
+  CGame* gameObj = getCGame();
+  cgame_postRedisplay(gameObj);
+}
+
+
+int main(int argc, char** argv) {
+  printf("call main\r\n");
+  //
+  CGame* gameObj = createCGame(400, 300);
+  cgame_setOnEnterFrameFunc(gameObj, _onEnterFrame);
+  //
+
+  CTtf* ttf = cttfMgr_createTtf(getCTtfMgr(), "examples/assets/Roboto-Bold.ttf", 22);
+
+  CDynaTexAtlas* atlas = cgame_getCDynaTexAtlas(gameObj, 0);
+  CDynaBlockSpace out;
+  ctexAtlas_addImageFromCTtf(atlas, ttf, "testtest", 1.0, 0.5, 0.5, 1.0, &out);
+
+
+  CImage* img = ctexAtlas_getImage(atlas);
+
+  sprite001 = (CObject3D*)createCSprite2D(50, 50);
+  csprite2d_setImage((CSprite2D*)sprite001, img, &out);
+
+  printf("call main A\r\n");
+  //
+  CObject3D* root = cgame_getRoot(gameObj);
+  cobject3d_addNode(root, (CObject3D*)sprite001);
+  ccamera3d_updateAtPerspective(cgame_getCamera(gameObj),
+      0.0, 0.0, 5.0,
+      0.0, 0.0, 0.0,
+      3.14*45.0/180.0, 400.0/300.0, 0.5, 1000.0);
+
+  cgame_run(gameObj);
+
+  return 0;
+}
